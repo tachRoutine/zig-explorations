6785ae2d6b2e357af2bb69f0b21fa5c6dd63c408
tacheraSasi <tacherasasi@gmail.com>
2026-07-06 17:01:23 +0300

    Read file contents and print to stdout

--- file_ops.zig
+++ file_ops.zig
@@ -1,6 +1,25 @@
 const std = @import("std");
+const String: type = []const u8;
 
-pub fn main(init: std.process.Init) void {
+pub fn main(init: std.process.Init) !void {
     const io = init.io;
-    std.debug.print("hello world\n", .{});
+    const allocator = init.gpa;
+    const cwd = std.Io.Dir.cwd();
+
+    const file = try cwd.openFile(io, "yoo.txt", .{});
+    defer file.close(io);
+
+    const stats = try file.stat(io);
+
+    const data = try allocator.alloc(u8, stats.size);
+    defer allocator.free(data);
+
+    const bytes_read = try file.readPositionalAll(io,data,0);
+    std.debug.print("{s}\n", .{data[0..bytes_read]});
+
+    // the easy approach
+    const datta = try cwd.readFileAlloc(io, "yoo.txt", allocator, .unlimited);
+    defer allocator.free(datta);
+
+    std.debug.print("{s}",.{datta});
 }
