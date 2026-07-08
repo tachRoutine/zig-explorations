7157a0d170080dc7c188e74cb211a9fc0fcdc258
tacheraSasi <tacherasasi@gmail.com>
2026-07-08 12:51:01 +0300

    Read JSON from file and serialize user to JSON
    Changed from hardcoded JSON string to reading "user.json" from disk,
    added file reading with proper cleanup, and implemented JSON
    serialization using std.json.fmt.

--- json.zig
+++ json.zig
@@ -1,23 +1,38 @@
 const std = @import("std");
 
-const User =  struct{
-    name: []const u8
-};
+const User = struct { name: []const u8 };
 
 const user_json =
     \\{"name":"Tach"}
 ;
 
 pub fn main(init: std.process.Init) !void {
-    const gpa = init.gpa;
-    const parsed_user = try std.json.parseFromSlice(User, gpa, user_json, .{});
-    defer parsed_user.deinit();
-    std.debug.print("user before: {s}\n",.{parsed_user.value.name});
+    const allocator = init.gpa;
+    const io = init.io;
+    const cwd = std.Io.Dir.cwd();
+    const file_path = "user.json";
 
-    var user1: User = .{
-        .name= "meee",
+    const json_data = try cwd.readFileAlloc(io, file_path, allocator, .unlimited);
+    defer allocator.free(json_data);
+
+    std.debug.print("raw data from {s} {s}", .{ file_path, json_data });
+    const parsed_user = try std.json.parseFromSlice(User, allocator, user_json, .{});
+    defer parsed_user.deinit();
+
+    std.debug.print("user before: {s}\n", .{parsed_user.value.name});
+
+    const user1: User = .{
+        .name = "meee",
     };
 
-    try std.json.Formatter(User).format();
-    
+    var buf: std.Io.Writer.Allocating = .init(allocator);
+    defer buf.deinit();
+
+    try buf.writer.print("{f}", .{std.json.fmt(user1, .{})});
+    const json_bytes = buf.written();
+    std.debug.print("After {s}\n", .{json_bytes});
 }
+
+// pub fn readJsonFile(io Io){
+
+// }
