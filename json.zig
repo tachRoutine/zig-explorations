967f86762caca88d33a2f773b0b9d6746568cbbe
tacheraSasi <tacherasasi@gmail.com>
2026-07-08 10:27:04 +0300

    Implement JSON parsing for User struct

--- json.zig
+++ json.zig
@@ -2,10 +2,15 @@ const std = @import("std");
 
 const User =  struct{
     name: []const u8
-}
+};
 
-const user_json = \\{"name":"Tach"}
+const user_json = 
+    \\{"name":"Tach"}
+;
 
 pub fn main(init: std.process.Init) !void {
-    
+    const gpa = init.gpa;
+    const parsed_user = try std.json.parseFromSlice(User, gpa, user_json, .{});
+    defer parsed_user.deinit();
+    std.debug.print("user: {s}\n",.{parsed_user.value.name});
 }
