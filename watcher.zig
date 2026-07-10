1f6705bfcaedd9f795b95f18411186db9720c65e
tacheraSasi <tacherasasi@gmail.com>
2026-07-10 12:40:10 +0300

    Replace io with allocator-based string allocation

--- watcher.zig
+++ watcher.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
-    const io = init.io;
+    // const io = init.io;
     const allocator = init.gpa;
+    const string = try allocator.create([]const u8);
+    defer allocator.destroy(string);
+    
 
-    std.debug.print("io:{any} \n\n allocator:{any}",.{io,allocator});
+    std.debug.print("string:{any} \n\n allocator:{any}",.{&string,allocator});
 }
