b17453666329925b145572d1065bb9c3b478be1a
tacheraSasi <tacherasasi@gmail.com>
2026-07-15 11:26:47 +0300

    Update io.zig

--- io.zig
+++ io.zig
@@ -2,18 +2,18 @@ const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
-    const read_buf: [4096]u8 = undefined;
-    const write_buf: [4096]u8 = undefined;
+    var read_buf: [4096]u8 = undefined;
+    var write_buf: [4096]u8 = undefined;
 
     const stdout_file = std.Io.File.stdout();
     const stdin_file = std.Io.File.stdin();
 
-    const  stdout_writer = stdout_file.writer(io,write_buf);
-    const  stdin_reader = stdin_file.writer(io,read_buf);
+    var  stdout_writer = stdout_file.writer(io,&write_buf);
+    var  stdin_reader = stdin_file.writer(io,&read_buf);
 
-    const stdout = &stdout_writer.interface;
+    var stdout = &stdout_writer.interface;
     _ = &stdin_reader.interface;
 
-    stdout.print("hello world",.{});
-    stdout.flush();
+    try stdout.print("hello world",.{});
+    try stdout.flush();
 }
